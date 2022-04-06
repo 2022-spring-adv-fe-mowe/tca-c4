329d3e4cde74233cb562381de047dd920cd2c40a
sushicat <alee@madisoncollege.edu>
2022-04-06 15:53:51 -0500

    get router working!

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export const Home = () => {
+
+    return (
+        <>
+        <h2>Home</h2>
+        <Button>Play</Button>
+        </>
+
+    );
+};
---
+++ src/Play.js
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+export const Play = () => {
+
+    return (
+        <>
+        <h2>Play</h2>
+        <Button>I WON</Button>
+        <Button>I LOST</Button>
+        </>
+
+    );
+};
