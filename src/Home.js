8b30202707703dba54ce30feb0db9d869ee1daa2
sushicat <alee@madisoncollege.edu>
2022-04-11 20:33:08 -0500

    Win and Lose Numbers, my Winning rate

--- src/Home.js
+++ src/Home.js
@@ -5,19 +5,33 @@ export const Home = ({gameResults}) => {
 
     const nav = useNavigate();
 
+    console.log(gameResults);
+
+    const winCount = gameResults.filter(n => n == true).length;
+    console.log(winCount);
+   
+    const loseCount = gameResults.length - winCount
+
+    const myWinningPersentage = ((winCount / gameResults.length)*100).toFixed(2) 
+
     return (
         <>
         <h2>Home</h2>
         <h3>Total game played: {gameResults.length} </h3>
+        <h3>Won game total: {winCount}</h3>
+        <h3>Lost game total: {loseCount}</h3>
+        <h3>My Winning Persentage: {myWinningPersentage}%</h3>
         <Button
             onClick = {()=>nav("/play")}
 
         >Play</Button>
       
-        {
+        {/* {
             gameResults.map(x => <div>{x ? "won" : "lost"}</div>)
-        }
-       
+        
+        } */}
+
+
         </>
        
     );
